33cdc2ace7c47909c1e997a203dbb25ac16ce9ea
Baran Karaaslan <barankaraaslan@protonmail.com>
2022-05-08 23:36:43 +0300

    change log message

--- src/liblog4cpp/log4cpp.cpp
+++ src/liblog4cpp/log4cpp.cpp
@@ -6,5 +6,5 @@
 #include <liblog4cpp/log4cpp.hpp>
 
 void log4cpp::log(std::string message) {
-    std::cout << "Logged by my beautiful library ::" << message << std::endl;
+    std::cout << "log4cpp::" << message << std::endl;
 }
